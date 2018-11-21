a8a63afc9ed9871538e5f99eb608dfa670bf14dd
David Hudson <im@davidhudson.me>
2018-11-21 08:01:08 -0600

    Add git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,10 +3,14 @@ alias ack='nocorrect ack'
 alias adb='nocorrect adb'
 alias repo='nocorrect repo'
 alias mux='nocorrect mux'
-
+alias ll='ls -l'
 alias sshkey="cat ~/.ssh/id_rsa.pub | pbcopy && echo 'Copied to clipboard.'"
-
 alias vi='vim'
+
+alias gst='git status'
+alias gd='git diff'
+alias gc='git commit'
+alias ga='git add'
 
 if [ "$OS_NAME" = "Darwin" ]; then
     alias ls='ls -G'
